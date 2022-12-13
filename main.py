0d3a292ac2e2e4f5c053d0001ffc30f84d31f06a
Pal_NV <nikolai.pal.89@gmail.com>
2022-12-13 16:16:25 +0300

    2

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ window = Tk()
 window.title("Рассчет неопределенности ММК")
 window.geometry('800x500')
 
-
 tab_control = ttk.Notebook(window)
 
 # первая вкладка тип анализа
@@ -22,22 +21,21 @@ lbl_2.grid(column=1, row=0)
 # переменная Tkinter
 chk_state_1 = BooleanVar()
 chk_state_2 = BooleanVar()
-chk_state_3 = BooleanVar()
-chk_state_4 = BooleanVar()
 #начальное состояние
 chk_state_1.set(False)
 chk_state_2.set(False)
-chk_state_3.set(False)
-chk_state_4.set(False)
-chk_1 = Checkbutton(tab1, text='Массовая доля в навеске', var=chk_state_1)
-chk_2 = Checkbutton(tab1, text='Массовая доля в растворе', var=chk_state_2)
-chk_3 = Checkbutton(tab1, text='Градуировка из навески', var=chk_state_3)
-chk_4 = Checkbutton(tab1, text='Градуировка из раствора', var=chk_state_4)
-chk_1.grid(column=0, row=1)
-chk_2.grid(column=0, row=2)
-chk_3.grid(column=1, row=1)
-chk_4.grid(column=1, row=2)
 
+# галочки (тут не подходят)
+# chk_1 = Checkbutton(tab1, text='Массовая доля в навеске', var=chk_state_1)
+# chk_2 = Checkbutton(tab1, text='Массовая доля в растворе', var=chk_state_2)
+# chk_3 = Checkbutton(tab1, text='Градуировка из навески', var=chk_state_3)
+# chk_4 = Checkbutton(tab1, text='Градуировка из раствора', var=chk_state_4)
+
+# создаем радиокнопки и сразу вставляем в вкладку
+Radiobutton(tab1, text='Массовая доля в навеске', var=chk_state_1, value=0).grid(column=0, row=1)
+Radiobutton(tab1, text='Массовая доля в растворе', var=chk_state_1, value=1).grid(column=0, row=2)
+Radiobutton(tab1, text='Градуировка из навески', var=chk_state_2, value=0).grid(column=1, row=1)
+Radiobutton(tab1, text='Градуировка из раствора', var=chk_state_2, value=1).grid(column=1, row=2)
 
 
 # вторая вкладка ввод данных
@@ -45,7 +43,25 @@ tab2 = ttk.Frame(tab_control)
 tab_control.add(tab2, text='Ввод данных')
 # текст 2 вкладки
 lbl = Label(tab2, text="Ввод первичных данных по пробам и градуировке", font=("Arial Bold", 15))
-lbl.grid(column=0, row=0)
+lbl.place(relx=0.05, rely=0.05)
+
+# програмка для кол-ва полей n
+def pole_n(event):
+
+    n_pole = int(combobox.get())
+    for i in range(n_pole):
+        ttk.Entry().pack()
+
+
+# выпадающий список
+number = [1,2,3,4,5,6,7,8,9,10]
+
+combobox = ttk.Combobox(tab2,  values=number)
+combobox.grid(column=0, row=2)
+
+combobox.bind("<<ComboboxSelected>>", pole_n)
+
+
 
 # третья вкладка настройки
 tab3 = ttk.Frame(tab_control)
