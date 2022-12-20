44381085cd8f825d4df87070df7b01d897da52f2
nicolai_pal <nikolai.pal.89@gmail.com>
2022-12-20 16:04:23 +0300

    Создание табличек

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ Radiobutton(tab1, text='Массовая доля в растворе', var=chk_
 Radiobutton(tab1, text='Градуировка из навески', var=chk_state_2, value=0).grid(column=1, row=1)
 Radiobutton(tab1, text='Градуировка из раствора', var=chk_state_2, value=1).grid(column=1, row=2)
 
+
+
+
 # вторая вкладка ввод данных
 tab2 = ttk.Frame(tab_control)
 tab_control.add(tab2, text='Ввод данных градуировка')
@@ -48,12 +51,74 @@ Label(tab2, text="Ввод первичных данных по градуиро
 Label(tab2, text="Введите количество анализируемых элементов", font=("Arial Bold", 10)).place(relx=0.05, rely=0.15)
 Label(tab2, text="Введите количество градуировочных растворов", font=("Arial Bold", 10)).place(relx=0.05, rely=0.21)
 
-# програмка для кол-ва полей n
-def pole_n(event):
 
-    n_pole = int(combobox.get())
-    for i in range(n_pole):
-        ttk.Entry().pack()
+# програмка для кол-ва полей n
+def pole_n():
+
+    """
+    програмка для кол-ва полей (в разработке)
+    """
+    global lab_el # создаем глобальную переменную для названия таблички массовой доли
+    lab_el = Label(tab2, text="Массовая доля", font=("Arial Bold", 10))
+    lab_el.place(relx=0.05, rely=0.31)
+
+    num_std = int(combobox_1.get())
+    num_el = int(combobox_2.get())
+
+    dob_0 = 0 # горизонтальный пропуск
+
+
+    for e in range(num_el):
+
+        globals()['lbl_el' + str(e)] = Label(tab2, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
+        globals()['lbl_el' + str(e)].place(relx=0.25+dob_0, rely=0.33)
+
+        dob_0 += 0.16
+
+        dob_1 = 0  # вертикальный пропуск между строк надписей
+        dob_2 = 0  # вертикальный пропуск между полями ввода
+
+        for i in range(num_std):
+
+            if 'lbl_std' + str(i) not in globals():
+                globals()['lbl_std' + str(i)] = Label(tab2, text=f'Стандарт_{i+1}', font=("Arial Bold", 8))
+                globals()['lbl_std' + str(i)].place(relx=0.05, rely=0.375+dob_1)
+
+            if 'lbl_ent' + str(e) + str(i) not in globals():
+                globals()['lbl_ent' + str(e) + str(i)] = ttk.Entry()
+                globals()['lbl_ent' + str(e) + str(i)].place(relx=0.05+dob_0, rely=0.40+dob_2)
+
+            dob_1 += 0.045
+            dob_2 += 0.043
+
+def pole_n_del():
+
+    """
+    програмка для удаления  полей (в разработке)
+    """
+
+    num_std = int(combobox_1.get())
+    num_el = int(combobox_2.get())
+
+    for e in range(num_el):
+
+        globals()['lbl_el' + str(e)].destroy()
+
+        for i in range(num_std):
+
+            if 'lbl_std' + str(i) in globals():
+                globals()['lbl_std' + str(i)].destroy()
+
+            if 'lbl_ent' + str(e) + str(i) in globals():
+                globals()['lbl_ent' + str(e) + str(i)].destroy()
+
+
+
+# кнопка при нажатии которой вылазит таблица, которую надо заполнить
+ttk.Button(tab2, text="Заполнить", command=pole_n).place(relx=0.67, rely=0.18)
+
+# кнопка удалении таблицы
+ttk.Button(tab2, text="Все удалить", command=pole_n_del).place(relx=0.80, rely=0.18)
 
 # выпадающий список
 number = [1,2,3,4,5,6,7,8,9,10]
@@ -64,7 +129,11 @@ combobox_1.place(relx=0.45, rely=0.15)
 combobox_2 = ttk.Combobox(tab2,  values=number)
 combobox_2.place(relx=0.45, rely=0.22)
 
-combobox_1.bind("<<ComboboxSelected>>", pole_n)
+
+
+
+
+#combobox_1.bind("<<ComboboxSelected>>", pole_n)
 
 # третья вкладка настройки
 tab3 = ttk.Frame(tab_control)
