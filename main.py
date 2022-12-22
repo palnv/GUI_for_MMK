f5f0ac98ee5831ed5c5f4b7dea38aaba43bac180
Pal_NV <nikolai.pal.89@gmail.com>
2022-12-22 17:28:25 +0300

    Разметка через place

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 
-
 from tkinter.ttk import Checkbutton
 
 """
-- решить проблему рамок
 - добавить возможность построения графиков с расчетом корелляции
 """
 
@@ -56,9 +54,20 @@ tab2 = ttk.Frame(tab_control)
 tab_control.add(tab2, text='Ввод данных градуировка')
 
 # текст 2 вкладки
-Label(tab2, text="Ввод первичных данных по градуировке", font=("Arial Bold", 15)).place(relx=0.05, rely=0.05)
-Label(tab2, text="Введите количество анализируемых элементов", font=("Arial Bold", 10)).place(relx=0.05, rely=0.15)
-Label(tab2, text="Введите количество градуировочных растворов", font=("Arial Bold", 10)).place(relx=0.05, rely=0.21)
+Label(tab2, text="Ввод первичных данных по градуировке", font=("Arial Bold", 15)).place(x=10, y=10)
+Label(tab2, text="Введите количество анализируемых элементов", font=("Arial Bold", 10)).place(x=10, y=70)
+Label(tab2, text="Введите количество градуировочных растворов", font=("Arial Bold", 10)).place(x=10, y=100)
+
+# выпадающий список
+number = [1,2,3,4,5,6,7,8,9,10]
+
+combobox_1 = ttk.Combobox(tab2,  values=number)
+combobox_1.place(x=340, y=70)
+combobox_1.current(1)
+
+combobox_2 = ttk.Combobox(tab2,  values=number)
+combobox_2.place(x=340, y=100)
+combobox_2.current(3)
 
 
 # програмка для кол-ва полей n
@@ -73,38 +82,79 @@ def pole_n():
 
     except: pass
 
-    global name_table # создаем глобальную переменную для названия таблички массовой доли
-    name_table = Label(tab2, text="Массовая доля", font=("Arial Bold", 10))
-    name_table.place(relx=0.05, rely=0.31)
-
     num_el = int(combobox_1.get())
     num_std = int(combobox_2.get())
 
-    dob_0 = 0 # горизонтальный пропуск
+    """
+    табличка добавления массовой доли
+    """
+    global name_table_mass_dol # создаем глобальную переменную для названия таблички массовой доли
+    name_table_mass_dol = Label(tab2, text="Массовая доля (%)", font=("Arial Bold", 10))
+    name_table_mass_dol.place(x=10, y=150)
 
+    gap_0 = 0 # горизонтальный пропуск для таблички массовой доли
 
     for e in range(num_el):
 
-        globals()['lbl_el' + str(e)] = Label(tab2, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
-        globals()['lbl_el' + str(e)].place(relx=0.25+dob_0, rely=0.33)
+        globals()['lbl_el_m_d' + str(e)] = Label(tab2, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
+        globals()['lbl_el_m_d' + str(e)].place(x=180+gap_0, y=180)
 
-        dob_0 += 0.16
+        gap_0 += 130
 
-        dob_1 = 0  # вертикальный пропуск между строк надписей
-        dob_2 = 0  # вертикальный пропуск между полями ввода
+        gap_1 = 0  # вертикальный пропуск для таблички массовой доли
 
         for i in range(num_std):
 
-            if 'lbl_std' + str(i) not in globals():
-                globals()['lbl_std' + str(i)] = Label(tab2, text=f'Стандарт_{i+1}', font=("Arial Bold", 8))
-                globals()['lbl_std' + str(i)].place(relx=0.05, rely=0.375+dob_1)
+            # создание таблички со стандартами
+            if 'lbl_std_m_d' + str(i) not in globals():
+                globals()['lbl_std_m_d' + str(i)] = Label(tab2, text=f'Стандарт_{i+1}', font=("Arial Bold", 8))
+                globals()['lbl_std_m_d' + str(i)].place(x=40, y=200+gap_1)
 
-            if 'lbl_ent' + str(e) + str(i) not in globals():
-                globals()['lbl_ent' + str(e) + str(i)] = ttk.Entry(tab2)
-                globals()['lbl_ent' + str(e) + str(i)].place(relx=0.05+dob_0, rely=0.40+dob_2)
+            if 'lbl_ent_m_d' + str(e) + str(i) not in globals():
+                globals()['lbl_ent_m_d' + str(e) + str(i)] = ttk.Entry(tab2)
+                globals()['lbl_ent_m_d' + str(e) + str(i)].place(x=10+gap_0, y=200+gap_1)
 
-            dob_1 += 0.045
-            dob_2 += 0.043
+            gap_1 += 23
+
+
+    gap_0_itog_md = gap_0  # запомним размеры таблички массовой доли по горизонтали
+    gap_0 = 0  # обнулим
+
+    gap_1_itog_md = gap_1  # запомним размеры таблички массовой доли по вертикали
+    gap_1 = 0  # обнулим
+
+
+    """
+    табличка добавления импульсов
+    """
+
+    global name_table_impuls  # создаем глобальную переменную для названия таблички массовой доли
+    name_table_impuls = Label(tab2, text="Импульсы (cps)", font=("Arial Bold", 10))
+    name_table_impuls.place(x=10, y=gap_1_itog_md + 250) # отталкиваемся от размеров предыдущей таблички
+
+    # for e in range(num_el):
+    #
+    #     globals()['lbl_el_imp' + str(e)] = Label(tab2, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
+    #     globals()['lbl_el_imp' + str(e)].place(x=180+gap_0, y=gap_1_itog_md + 180)
+    #
+    #     gap_0 += 130
+    #
+    #     for i in range(num_std):
+    #
+    #         # создание таблички со стандартами
+    #         if 'lbl_std' + str(i) not in globals():
+    #             globals()['lbl_std_imp' + str(i)] = Label(tab2, text=f'Стандарт_{i+1}', font=("Arial Bold", 8))
+    #             globals()['lbl_std_imp' + str(i)].place(x=40, y=gap_1_itog_md + 200+gap_1)
+    #
+    #         if 'lbl_ent' + str(e) + str(i) not in globals():
+    #             globals()['lbl_ent_imp' + str(e) + str(i)] = ttk.Entry(tab2)
+    #             globals()['lbl_ent_imp' + str(e) + str(i)].place(x=10+gap_0, y=gap_1_itog_md + 200+gap_1)
+    #
+    #         gap_1 += 23
+
+
+
+
 
 def pole_n_del():
 
@@ -112,52 +162,51 @@ def pole_n_del():
     програмка для удаления  полей (в разработке)
     """
 
-    num_std = int(combobox_1.get())
-    num_el = int(combobox_2.get())
+    #num_std = int(combobox_1.get())
+    #num_el = int(combobox_2.get())
 
-    name_table.destroy()
+    # удаляем табличку массовой доли
+    name_table_mass_dol.destroy()
 
     for e in range(10):
 
-        if 'lbl_el' + str(e) in globals():
-
-            globals()['lbl_el' + str(e)].destroy()
+        if 'lbl_el_m_d' + str(e) in globals():
+            globals()['lbl_el_m_d' + str(e)].destroy()
             try:
-                del globals()['lbl_std' + str(i)]
+                del globals()['lbl_std_m_d' + str(e)]
             except: pass
+
+        # if 'lbl_el_imp' + str(e) in globals():
+        #     globals()['lbl_el_imp' + str(e)].destroy()
+        #     try:
+        #         del globals()['lbl_std_imp' + str(e)]
+        #     except: pass
 
         for i in range(10):
 
-            if 'lbl_std' + str(i) in globals():
-                globals()['lbl_std' + str(i)].destroy()
+            if 'lbl_std_m_d' + str(i) in globals():
+                globals()['lbl_std_m_d' + str(i)].destroy()
                 try:
-                    del globals()['lbl_std' + str(i)]
+                    del globals()['lbl_std_m_d' + str(i)]
                 except: pass
 
-            if 'lbl_ent' + str(e) + str(i) in globals():
-                globals()['lbl_ent' + str(e) + str(i)].destroy()
+            if 'lbl_ent_m_d' + str(e) + str(i) in globals():
+                globals()['lbl_ent_m_d' + str(e) + str(i)].destroy()
                 try:
-                    del globals()['lbl_ent' + str(e) + str(i)]
+                    del globals()['lbl_ent_m_d' + str(e) + str(i)]
                 except: pass
+
+    # удаляем табличку импульсов
+    name_table_impuls.destroy()
+
 
 
 
 # кнопка при нажатии которой вылазит таблица, которую надо заполнить
-ttk.Button(tab2, text="Заполнить", command=pole_n).place(relx=0.67, rely=0.18)
+ttk.Button(tab2, text="Заполнить", command=pole_n).place(x=510, y=85)
 
 # кнопка удалении таблицы
-ttk.Button(tab2, text="Все удалить", command=pole_n_del).place(relx=0.80, rely=0.18)
-
-# выпадающий список
-number = [1,2,3,4,5,6,7,8,9,10]
-
-combobox_1 = ttk.Combobox(tab2,  values=number)
-combobox_1.place(relx=0.45, rely=0.15)
-
-combobox_2 = ttk.Combobox(tab2,  values=number)
-combobox_2.place(relx=0.45, rely=0.22)
-
-
+ttk.Button(tab2, text="Все удалить", command=pole_n_del).place(x=610, y=85)
 
 
 
