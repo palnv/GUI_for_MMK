ae2cfeb706f226a7b1c887c61a0084e08be95aea
Pal_NV <nikolai.pal.89@gmail.com>
2023-01-18 18:48:52 +0300

    редактирование таблицы проб

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from tkinter.ttk import Checkbutton
 - добавить возможность построения графиков с расчетом корелляции
 """
 
-#новая ветка для оптимизации
-
 # начальное окно
 window = Tk()
 window.title("Рассчет неопределенности ММК")
@@ -355,11 +353,11 @@ tab_control.add(tab3, text='Ввод данных проб')
 # текст 3 вкладки
 Label(tab3, text="Ввод первичных данных по пробам", font=("Arial Bold", 15)).place(x=10, y=10)
 Label(tab3, text="Введите количество анализируемых элементов", font=("Arial Bold", 10)).place(x=10, y=70)
-Label(tab3, text="Введите количество градуировочных растворов", font=("Arial Bold", 10)).place(x=10, y=100)
+Label(tab3, text="Введите количество анализируемых проб", font=("Arial Bold", 10)).place(x=10, y=100)
 
 combobox_3 = ttk.Combobox(tab3,  values=v.number)
 combobox_3.place(x=340, y=70)
-combobox_3.current(1)
+combobox_3.current(0)
 
 combobox_4 = ttk.Combobox(tab3,  values=v.number)
 combobox_4.place(x=340, y=100)
@@ -368,7 +366,7 @@ combobox_4.current(3)
 def pole_n_proba():
 
     """
-    програмка добавления полей заполнения во вкладку ввод данных градуировка
+    програмка добавления полей заполнения во вкладку ввод данных проба
     """
 
     try:
@@ -380,18 +378,18 @@ def pole_n_proba():
     num_std = int(combobox_4.get())
 
     """
-    табличка добавления массовой доли
+    табличка добавления плотности проб
     """
-    global name_table_mass_dol_proba # создаем глобальную переменную для названия таблички массовой доли
-    name_table_mass_dol_proba = Label(tab3, text="Массовая доля (%)", font=("Arial Bold", 10))
-    name_table_mass_dol_proba.place(x=10, y=140)
+    global name_table_plotn_proba # создаем глобальную переменную для названия таблички плотности
+    name_table_plotn_proba = Label(tab3, text="Плотность растворов (г/см.куб)", font=("Arial Bold", 10))
+    name_table_plotn_proba.place(x=10, y=140)
 
     gap_0 = 0 # горизонтальный пропуск для таблички массовой доли
 
     for e in range(num_el):
 
-        globals()['lbl_el_m_d_proba' + str(e)] = Label(tab3, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
-        globals()['lbl_el_m_d_proba' + str(e)].place(x=120+gap_0, y=160)
+        globals()['lbl_el_pl_proba' + str(e)] = Label(tab3, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
+        globals()['lbl_el_pl_proba' + str(e)].place(x=120+gap_0, y=160)
 
         gap_0 += v.gap_gor_add
 
@@ -400,13 +398,13 @@ def pole_n_proba():
         for i in range(num_std):
 
             # создание таблички со стандартами
-            if 'lbl_std_m_d_proba' + str(i) not in globals():
-                globals()['lbl_std_m_d_proba' + str(i)] = Label(tab3, text=f'Стандарт_{i+1}', font=("Arial Bold", 8))
-                globals()['lbl_std_m_d_proba' + str(i)].place(x=40, y=180+gap_1)
+            if 'lbl_std_pl_proba' + str(i) not in globals():
+                globals()['lbl_std_pl_proba' + str(i)] = Label(tab3, text=f'Проба_{i+1}', font=("Arial Bold", 8))
+                globals()['lbl_std_pl_proba' + str(i)].place(x=40, y=180+gap_1)
 
-            if 'lbl_ent_m_d_proba' + str(e) + str(i) not in globals():
-                globals()['lbl_ent_m_d_proba' + str(e) + str(i)] = ttk.Entry(tab3, width = 10)
-                globals()['lbl_ent_m_d_proba' + str(e) + str(i)].place(x=50+gap_0, y=180+gap_1)
+            if 'lbl_ent_pl_proba' + str(e) + str(i) not in globals():
+                globals()['lbl_ent_pl_proba' + str(e) + str(i)] = ttk.Entry(tab3, width = 10)
+                globals()['lbl_ent_pl_proba' + str(e) + str(i)].place(x=50+gap_0, y=180+gap_1)
 
             gap_1 += v.gap_vert_add
 
@@ -426,7 +424,7 @@ def pole_n_proba():
 
     for e in range(num_el):
 
-        globals()['lbl_el_imp_proba' + str(e)] = Label(tab3, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
+        globals()['lbl_el_imp_proba' + str(e)] = Label(tab3, text=f'Проба_{e + 1}', font=("Arial Bold", 8))
         globals()['lbl_el_imp_proba' + str(e)].place(x=gap_0_itog_md+250 + gap_0, y=160)
 
         gap_0 += v.gap_gor_add
@@ -436,7 +434,7 @@ def pole_n_proba():
         for i in range(num_std):
 
             if 'lbl_std_imp_proba' + str(i) not in globals():
-                globals()['lbl_std_imp_proba' + str(i)] = Label(tab3, text=f'Стандарт_{i + 1}', font=("Arial Bold", 8))
+                globals()['lbl_std_imp_proba' + str(i)] = Label(tab3, text=f'Проба_{i + 1}', font=("Arial Bold", 8))
                 globals()['lbl_std_imp_proba' + str(i)].place(x=gap_0_itog_md+170, y=180+gap_2)
 
             if 'lbl_ent_imp_proba' + str(e) + str(i) not in globals():
@@ -457,7 +455,7 @@ def pole_n_proba():
 
     for e in range(num_el):
 
-        globals()['lbl_el_razb_proba' + str(e)] = Label(tab3, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
+        globals()['lbl_el_razb_proba' + str(e)] = Label(tab3, text=f'Проба_{e + 1}', font=("Arial Bold", 8))
         globals()['lbl_el_razb_proba' + str(e)].place(x=120+gap_0, y=gap_1_itog_md + 230)
 
         gap_0 += v.gap_gor_add
@@ -467,7 +465,7 @@ def pole_n_proba():
         for i in range(num_std):
 
             if 'lbl_std_razb_proba' + str(i) not in globals():
-                globals()['lbl_std_razb_proba' + str(i)] = Label(tab3, text=f'Стандарт_{i+1}', font=("Arial Bold", 8))
+                globals()['lbl_std_razb_proba' + str(i)] = Label(tab3, text=f'Проба_{i+1}', font=("Arial Bold", 8))
                 globals()['lbl_std_razb_proba' + str(i)].place(x=40, y=gap_1_itog_md + 250+gap_3)
 
             if 'lbl_ent_razb_proba' + str(e) + str(i) not in globals():
@@ -489,7 +487,7 @@ def pole_n_proba():
 
     for e in range(num_el):
 
-        globals()['lbl_el_rsd_proba' + str(e)] = Label(tab3, text=f'Элемент_{e + 1}', font=("Arial Bold", 8))
+        globals()['lbl_el_rsd_proba' + str(e)] = Label(tab3, text=f'Проба_{e + 1}', font=("Arial Bold", 8))
         globals()['lbl_el_rsd_proba' + str(e)].place(x=gap_0_itog_md+250 + gap_0, y=gap_1_itog_md + 230)
 
         gap_0 += v.gap_gor_add
@@ -499,7 +497,7 @@ def pole_n_proba():
         for i in range(num_std):
 
             if 'lbl_std_rsd_proba' + str(i) not in globals():
-                globals()['lbl_std_rsd_proba' + str(i)] = Label(tab3, text=f'Стандарт_{i + 1}', font=("Arial Bold", 8))
+                globals()['lbl_std_rsd_proba' + str(i)] = Label(tab3, text=f'Проба_{i + 1}', font=("Arial Bold", 8))
                 globals()['lbl_std_rsd_proba' + str(i)].place(x=gap_0_itog_md+170, y=gap_1_itog_md + 250 + gap_3)
 
             if 'lbl_ent_rsd_proba' + str(e) + str(i) not in globals():
@@ -509,10 +507,10 @@ def pole_n_proba():
             gap_3 += v.gap_vert_add
 
     """
-    табличка растворения ГСО (перевода в пробирки)
+    табличка растворения проб (перевода в пробирки)
     """
     global name_table_probirka_proba  # название таблички
-    name_table_probirka_proba = Label(tab3, text="Растворение ГСО", font=("Arial Bold", 10))
+    name_table_probirka_proba = Label(tab3, text="Растворение проб", font=("Arial Bold", 10))
     name_table_probirka_proba.place(x=10, y=gap_3_itog_razb + 280)
 
     gap_6 = 0
@@ -549,7 +547,7 @@ def pole_n_proba():
         for i in range(num_std):
 
             if 'lbl_std_probirka_proba' + str(i) not in globals():
-                globals()['lbl_std_probirka_proba' + str(i)] = Label(tab3, text=f'Стандарт_{i + 1}', font=("Arial Bold", 8))
+                globals()['lbl_std_probirka_proba' + str(i)] = Label(tab3, text=f'Проба_{i + 1}', font=("Arial Bold", 8))
                 globals()['lbl_std_probirka_proba' + str(i)].place(x=40 + gap_6, y=gap_3_itog_razb + 340 + gap_3)
 
             gap_3 += v.gap_vert_add
@@ -564,7 +562,7 @@ def pole_n_del_proba():
 
     # удаляем таблички
 
-    name_table_mass_dol_proba.destroy()
+    name_table_plotn_proba.destroy()
     name_table_razb_proba.destroy()
     name_table_imp_proba.destroy()
     name_table_rsd_proba.destroy()
@@ -572,7 +570,7 @@ def pole_n_del_proba():
     # названия колонок
     name_table_probirka_proba.destroy()
 
-    tabl_del_1 = ['lbl_el_m_d_proba', 'lbl_el_razb_proba', 'lbl_el_imp_proba', 'lbl_el_rsd_proba', 'lbl_el_probirk_proba']
+    tabl_del_1 = ['lbl_el_pl_proba', 'lbl_el_razb_proba', 'lbl_el_imp_proba', 'lbl_el_rsd_proba', 'lbl_el_probirk_proba']
 
     for e in range(10):
 
@@ -610,7 +608,7 @@ def pole_n_del_proba():
                         pass
 
         # название всех табличек
-        tabl_del_2 = ['lbl_std_m_d_proba','lbl_ent_m_d_proba','lbl_std_razb_proba','lbl_ent_razb_proba',\
+        tabl_del_2 = ['lbl_std_pl_proba','lbl_ent_pl_proba','lbl_std_razb_proba','lbl_ent_razb_proba',\
                     'lbl_std_imp_proba','lbl_ent_imp_proba','lbl_std_rsd_proba','lbl_ent_rsd_proba','lbl_std_probirka_proba']
 
         for i in range(10):
